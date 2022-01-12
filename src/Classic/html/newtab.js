5483e9bd7f076c634f2920397d5260c05d48fec6
Victor Baconnet <victor.baconnet06@gmail.com>
2022-01-12 11:01:42 -0800

    Fancy new tab html with search bar

---
+++ src/Classic/html/newtab.js
@@ -0,0 +1,13 @@
+const f = document.getElementById('form');
+const q = document.getElementById('query');
+const google = 'https://www.google.com/search?q=';
+
+function submitted(event) {
+  event.preventDefault();
+  const url = google + '+' + q.value;
+  location.href = url;
+  //const win = window.open(url, '_blank');
+  //win.focus();
+}
+
+f.addEventListener('submit', submitted);
---
+++ translations/ts_files/omicron_fr.ts
@@ -0,0 +1,130 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>FenPrincipale</name>
+    <message>
+        <location filename="FenPrincipale.cpp" line="50"/>
+        <source>Close</source>
+        <translation>Quitter</translation>
+    </message>
+    <message>
+        <location filename="FenPrincipale.cpp" line="51"/>
+        <source>Ctrl+Q</source>
+        <comment>Quit</comment>
+        <translation>Ctrl+Q</translation>
+    </message>
+    <message>
+        <location filename="FenPrincipale.cpp" line="54"/>
+        <source>New tab</source>
+        <translation>Nouvel onglet</translation>
+    </message>
+    <message>
+        <location filename="FenPrincipale.cpp" line="55"/>
+        <source>Ctrl+T</source>
+        <comment>New tab</comment>
+        <translation>Ctrl+T</translation>
+    </message>
+    <message>
+        <location filename="FenPrincipale.cpp" line="57"/>
+        <source>Previous</source>
+        <translation>Précédent</translation>
+    </message>
+    <message>
+        <location filename="FenPrincipale.cpp" line="59"/>
+        <source>Ctrl+Shift+P</source>
+        <comment>Previous page</comment>
+        <translation>Ctrl+Shift+P</translation>
+    </message>
+    <message>
+        <location filename="FenPrincipale.cpp" line="61"/>
+        <source>Next</source>
+        <translation>Suivant</translation>
+    </message>
+    <message>
+        <location filename="FenPrincipale.cpp" line="63"/>
+        <source>Ctrl+Shift+N</source>
+        <comment>Next page</comment>
+        <translation>Ctrl+Shift+N</translation>
+    </message>
+    <message>
+        <location filename="FenPrincipale.cpp" line="65"/>
+        <source>Home</source>
+        <translation>Accueil</translation>
+    </message>
+    <message>
+        <location filename="FenPrincipale.cpp" line="67"/>
+        <source>Ctrl+H</source>
+        <comment>Home</comment>
+        <translation>Ctrl+H</translation>
+    </message>
+    <message>
+        <location filename="FenPrincipale.cpp" line="69"/>
+        <source>Refresh</source>
+        <translation>Rafraîchir</translation>
+    </message>
+    <message>
+        <location filename="FenPrincipale.cpp" line="71"/>
+        <source>F4</source>
+        <comment>Refresh</comment>
+        <translation>F4</translation>
+    </message>
+    <message>
+        <location filename="FenPrincipale.cpp" line="73"/>
+        <source>Interrupt loading</source>
+        <translation>Interrompre</translation>
+    </message>
+    <message>
+        <location filename="FenPrincipale.cpp" line="75"/>
+        <source>Ctrl+Shift+S</source>
+        <comment>Stop page loading</comment>
+        <translation>Ctrl+Shift+S</translation>
+    </message>
+    <message>
+        <location filename="FenPrincipale.cpp" line="77"/>
+        <source>Search</source>
+        <translation>Rechercher</translation>
+    </message>
+    <message>
+        <location filename="FenPrincipale.cpp" line="81"/>
+        <source>Hide menu bar</source>
+        <translation>Cacher le menu</translation>
+    </message>
+    <message>
+        <location filename="FenPrincipale.cpp" line="97"/>
+        <source>&amp;File</source>
+        <translation>&amp;Fichier</translation>
+    </message>
+    <message>
+        <location filename="FenPrincipale.cpp" line="102"/>
+        <source>&amp;Navigation</source>
+        <translation>&amp;Navigation</translation>
+    </message>
+    <message>
+        <location filename="FenPrincipale.cpp" line="116"/>
+        <source>Navigation</source>
+        <translation>Navigation</translation>
+    </message>
+    <message>
+        <location filename="FenPrincipale.cpp" line="135"/>
+        <location filename="FenPrincipale.cpp" line="242"/>
+        <source>https://www.google.com</source>
+        <translation>https://google.fr</translation>
+    </message>
+    <message>
+        <location filename="FenPrincipale.cpp" line="226"/>
+        <source>(New tab)</source>
+        <translation>(Nouvel onglet)</translation>
+    </message>
+    <message>
+        <location filename="FenPrincipale.cpp" line="305"/>
+        <source>Error while loading</source>
+        <translation>Erreur de chargement</translation>
+    </message>
+    <message>
+        <location filename="FenPrincipale.cpp" line="308"/>
+        <source>Done.</source>
+        <translation>Terminé.</translation>
+    </message>
+</context>
+</TS>
